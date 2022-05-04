7f3ca0fce8f88658c5ac9873464e469b63b45d3c
Bill Prucka <pruckawi@gmail.com>
2022-05-03 22:34:03 -0400

    Daily commit

--- R/binary_example.R
+++ R/binary_example.R
@@ -1,11 +1,18 @@
 library(tidyverse)
 library(titanic)
+library(pROC)
 
 titanic_dt <- titanic_train
+titanicTest_dt <- titanic_test
 
 titanic_dt %>% distinct(Sex)
 titanic_dt %>% group_by(Sex,Pclass) %>% summarize(mean(Fare))
 titanic_dt %>% group_by(Sex,Pclass) %>% count()
 
-glm(Survived ~ Sex + Pclass,data=titanic_dt) %>% summary()
+glmObj <- glm(Survived ~ Sex + Pclass,data=titanic_dt,family=binomial)
+
+yPred <- predict(glmObj,newdata=titanic_dt)
+titanic_dt <- titanic_dt %>% mutate(SurvivedPred = as.numeric(yPred))
+curveROC <- roc(titanic_dt,Survived, SurvivedPred)
+plot(curveROC)
 
